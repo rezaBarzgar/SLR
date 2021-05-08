1aa0baa622f690454506deee6b372d116d7f3a21
Mohammadreza Pourreza <71866535+MohammadrezaPourreza@users.noreply.github.com>
2021-05-08 13:59:24 +0430

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,17 +1,26 @@
 from quickumls import QuickUMLS
 import spacy
 import subprocess
+import glob
+import numpy as np
 
-
+np_load_old = np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+topics = np.load('topics.npy')
+# output = np.load('output_quickumls.npy')
+# print(output)
 matcher = QuickUMLS('../SLR/QuickUMLS')
-#text = "The ulna has dislocated posteriorly from the trochlea of the humerus."
-text = "Plasma testosterone measurement humerus"
-result = matcher.match(text, best_match=True, ignore_syntax=False)
-cui = []
-for item in result:
-    cui.append(item[0]['cui'])
-with open('../QueryExpantion/cuis.txt','w') as file:
-    for item in cui:
-        file.write(item+'\n')
-    file.close()
+output = []
+for i in range(len(topics)):
+    res = []
+    print(topics[i,1])
+    result = matcher.match(topics[i,1], best_match=True, ignore_syntax=False)
+    cui = []
+    for item in result:
+        cui.append(item[0]['cui'])
+    res.append(topics[i,0])
+    res.append(cui)
+    output.append(res)
+arr = np.array(output,dtype=object)
+np.save('output_quickumls',arr)
 
